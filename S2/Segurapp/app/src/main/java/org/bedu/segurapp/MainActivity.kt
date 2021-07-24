8524b9e474ce89d87cfc005a175167b51f83de77
Oscar <krz.oficial@gmail.com>
2021-07-23 21:28:46 -0500

    agregado de formulario para registro

--- S2/Segurapp/app/src/main/java/org/bedu/segurapp/MainActivity.kt
+++ S2/Segurapp/app/src/main/java/org/bedu/segurapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.bedu.segurapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btCreate: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btCreate=findViewById(R.id.btCreate)
+
+        btCreate.setOnClickListener {
+
+            intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
