bd73400cd97f1e4027e23030e858f194efc18976
Antonio Labra <antonio_labra@hotmail.com>
2021-07-28 02:48:34 -0500

    Creacion de archivo Helpers.kt y metodos para obtener nombre y telefono de lista de contactos

---
+++ S3/Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/SafeContactsActivity.kt
@@ -0,0 +1,11 @@
+package org.bedu.segurapp.ui.getStarted
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class safeContactsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_safe_contacts)
+    }
+}
