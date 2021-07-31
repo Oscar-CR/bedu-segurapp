459d635f99ea72e4e0beb40002407481bb0f4d2e
Oscar <krz.oficial@gmail.com>
2021-07-30 19:39:25 -0500

    Agregado de la vista de contactos, mediante RecyclerView

---
+++ S4/Segurapp/app/src/main/java/org/bedu/segurapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package org.bedu.segurapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import org.bedu.segurapp.ui.MenuActivity
+import org.bedu.segurapp.ui.RegisterActivity
+import org.bedu.segurapp.ui.getStarted.GetStartedActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btCreate: Button
+    private lateinit var btnSubmit: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initComponents()
+
+        btCreate.setOnClickListener {
+            intent = Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSubmit.setOnClickListener{
+            intent = Intent(applicationContext, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun initComponents(){
+        btnSubmit = findViewById(R.id.btnSubmit)
+        btCreate=findViewById(R.id.btCreate)
+    }
+}
