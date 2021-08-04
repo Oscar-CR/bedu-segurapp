0f8a5b04f8558c179ed214b3368cb97a70f41576
Antonio Labra <antonio_labra@hotmail.com>
2021-08-04 02:13:14 -0500

    Login Activity.kt con información previa de Main Activity.kt

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/login/LoginActivity.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/login/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import org.bedu.segurapp.R
+import org.bedu.segurapp.ui.forgotPassword.BottomSheetForgotPassword
 import org.bedu.segurapp.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var btCreate: Button
+    private lateinit var tvForgotPassword: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +23,16 @@ class LoginActivity : AppCompatActivity() {
             intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        tvForgotPassword.setOnClickListener {
+            BottomSheetForgotPassword().apply {
+                show(supportFragmentManager, tag)
+            }
+        }
     }
 
     private fun initComponents(){
-        btCreate=findViewById(R.id.btCreate)
+        btCreate = findViewById(R.id.btCreate)
+        tvForgotPassword = findViewById(R.id.tvForgotPassword)
     }
 }
