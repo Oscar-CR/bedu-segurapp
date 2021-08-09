3963ff9079e1541ccf328ccb71dc14ff136966e0
Antonio Labra <antonio_labra@hotmail.com>
2021-08-09 01:46:33 -0500

    Enlazando GetStartedDoneFragment a ViewPagerAdapter

--- Segurapp/app/src/main/java/org/bedu/segurapp/adapters/ViewPagerAdapter.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/adapters/ViewPagerAdapter.kt
@@ -3,19 +3,21 @@ package org.bedu.segurapp.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import org.bedu.segurapp.ui.getStarted.GetStartedDoneFragment
 import org.bedu.segurapp.ui.getStarted.GetStartedProfileConfigurationFragment
 import org.bedu.segurapp.ui.getStarted.GetStartedSafeContactsFragment
 import org.bedu.segurapp.ui.getStarted.GetStartedSplashFragment
 
 class ViewPagerAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> GetStartedSplashFragment()
-            1->  GetStartedProfileConfigurationFragment()
+            1 -> GetStartedProfileConfigurationFragment()
             2 -> GetStartedSafeContactsFragment()
+            3 -> GetStartedDoneFragment()
             else -> {
                 GetStartedSplashFragment()
             }
