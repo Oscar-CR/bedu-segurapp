13f0d2f61f9b94ad8e3a205db250e7fe68e41cca
Antonio Labra <antonio_labra@hotmail.com>
2021-08-09 00:48:21 -0500

    Enlazando RegisterActivity.kt con GetStartedActivity.kt

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/register/RegisterActivity.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/register/RegisterActivity.kt
@@ -1,12 +1,26 @@
 package org.bedu.segurapp.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import org.bedu.segurapp.R
+import org.bedu.segurapp.ui.getStarted.GetStartedActivity
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var btSubmit: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        btSubmit = findViewById(R.id.btSubmit)
+        registerProcess()
+    }
+
+    fun registerProcess(){
+        btSubmit.setOnClickListener {
+            val intent = Intent(this, GetStartedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
