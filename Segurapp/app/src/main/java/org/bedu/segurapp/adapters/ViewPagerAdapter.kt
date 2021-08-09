42be4c2884853f549f34612efb85cb2a49e1c51c
Antonio Labra <antonio_labra@hotmail.com>
2021-08-09 00:55:25 -0500

    Enlazando GetStartedSafeContactsFragment.kt dentro del ViewPagerAdapter.kt

--- Segurapp/app/src/main/java/org/bedu/segurapp/adapters/ViewPagerAdapter.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/adapters/ViewPagerAdapter.kt
@@ -4,16 +4,18 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.bedu.segurapp.ui.getStarted.GetStartedProfileConfigurationFragment
+import org.bedu.segurapp.ui.getStarted.GetStartedSafeContactsFragment
 import org.bedu.segurapp.ui.getStarted.GetStartedSplashFragment
 
 class ViewPagerAdapter(fragment: FragmentActivity) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> GetStartedSplashFragment()
             1->  GetStartedProfileConfigurationFragment()
+            2 -> GetStartedSafeContactsFragment()
             else -> {
                 GetStartedSplashFragment()
             }
