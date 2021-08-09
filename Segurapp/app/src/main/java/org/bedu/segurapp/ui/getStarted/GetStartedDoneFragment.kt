667920bc25fc9c044148529209311880eb37560d
Antonio Labra <antonio_labra@hotmail.com>
2021-08-09 02:06:50 -0500

    Creacion de GeStartedDoneFragment.kt con diseño y redireccion y Menu Principal

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedDoneFragment.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedDoneFragment.kt
@@ -1,21 +1,50 @@
 package org.bedu.segurapp.ui.getStarted
 
+import android.animation.ValueAnimator
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 import org.bedu.segurapp.R
+import org.bedu.segurapp.ui.home.HomeActivity
 
 
 class GetStartedDoneFragment : Fragment() {
-
+    private lateinit var mainAnimation: LottieAnimationView
+    private lateinit var btnAccept: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_started_done, container, false)
+        val view = inflater.inflate(R.layout.fragment_get_started_done, container, false)
+        initComponents(view)
+        setAnimation()
+        btnAcceptClickListener()
+        return view
+    }
+
+    private fun initComponents(view: View) {
+        mainAnimation = view.findViewById(R.id.main_animation)
+        btnAccept = view.findViewById(R.id.btn_accept)
+    }
+
+    private fun setAnimation() {
+        mainAnimation.setAnimation("done.json")
+        mainAnimation.playAnimation()
+        mainAnimation.repeatCount = ValueAnimator.INFINITE
+    }
+
+    private fun btnAcceptClickListener() {
+        btnAccept.setOnClickListener {
+            val intent = Intent(requireActivity(), HomeActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
     }
 }
