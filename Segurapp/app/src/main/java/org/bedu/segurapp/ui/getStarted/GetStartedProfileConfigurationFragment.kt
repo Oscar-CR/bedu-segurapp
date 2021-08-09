1d217984b9593581222e067464269371468e7a43
Antonio Labra <antonio_labra@hotmail.com>
2021-08-09 02:07:56 -0500

    Programar acción de redirección a la siguiente view en GetStartedProfileContigurationFragment y GetStartedSafeContactsFragment.kt

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedProfileConfigurationFragment.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedProfileConfigurationFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import org.bedu.segurapp.R
+import org.bedu.segurapp.helpers.moveNext
 
 
 class GetStartedProfileConfigurationFragment : Fragment() {
+    private lateinit var btnNext: Button
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_get_started_profile_configuration, container, false)
-
+        initComponents(view)
+        btnContinueClickListener()
         return view
+    }
+
+    private fun initComponents(view: View){
+        btnNext = view.findViewById(R.id.btn_next)
+    }
+
+    private fun btnContinueClickListener() {
+        btnNext.setOnClickListener {
+            moveNext(requireActivity() as GetStartedActivity)
+        }
     }
 }
 
--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedSafeContactsFragment.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedSafeContactsFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import org.bedu.segurapp.R
+import org.bedu.segurapp.helpers.moveNext
 
 
 class GetStartedSafeContactsFragment : Fragment() {
+
+    private lateinit var btnNext : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_get_started_safe_contacts, container, false)
+        val view = inflater.inflate(R.layout.fragment_get_started_safe_contacts, container, false)
+        initComponents(view)
+        btnContinueClickListener()
+        return view
+    }
+
+    private fun initComponents(view: View){
+        btnNext = view.findViewById(R.id.btn_next)
+    }
+
+    private fun btnContinueClickListener(){
+        moveNext(requireActivity() as GetStartedActivity)
     }
 
 }
