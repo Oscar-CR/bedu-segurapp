935b47717ee70ccdaef8c743153adac217c05602
Antonio Labra <antonio_labra@hotmail.com>
2021-08-10 02:06:32 -0500

    Agregando hideKeyboard() dentro de UIHelpers

--- Segurapp/app/src/main/java/org/bedu/segurapp/helpers/UIHelpers.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/helpers/UIHelpers.kt
@@ -1,5 +1,8 @@
 package org.bedu.segurapp.helpers
 
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.viewpager2.widget.ViewPager2
 
 fun moveNext(mPager: ViewPager2) {
@@ -8,4 +11,9 @@ fun moveNext(mPager: ViewPager2) {
 
 fun movePrevious(mPager: ViewPager2) {
     mPager.currentItem = mPager.currentItem - 1
+}
+
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
 }
