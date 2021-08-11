82983ac5767a282f799517402bdea2766dd49b55
Antonio Labra <antonio_labra@hotmail.com>
2021-08-11 01:37:27 -0500

    Creación de PermissionHelpers.kt donde se utiliza una función para abrir las configuraciones de la aplicación para asignar permisos de la aplicación manualmente

--- Segurapp/app/src/main/java/org/bedu/segurapp/helpers/PermissionHelpers.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/helpers/PermissionHelpers.kt
@@ -1,2 +1,15 @@
 package org.bedu.segurapp.helpers
 
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.fragment.app.FragmentActivity
+import org.bedu.segurapp.R
+
+fun openAppPermissionsScreen(activity: FragmentActivity){
+    val intent = Intent()
+    intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+    val uri: Uri = Uri.fromParts("package", activity.getString(R.string.package_name), null)
+    intent.data = uri
+    activity.startActivity(intent)
+}
