47027acf58ee276b3f4b0e58621512d79b939ee1
Antonio Labra <antonio_labra@hotmail.com>
2021-08-11 18:25:07 -0500

    Creacion de funcion que devuelve los permisos necesarios dentro de la aplicación

--- Segurapp/app/src/main/java/org/bedu/segurapp/helpers/PermissionHelpers.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/helpers/PermissionHelpers.kt
@@ -1,15 +1,22 @@
 package org.bedu.segurapp.helpers
 
+import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import androidx.fragment.app.FragmentActivity
 import org.bedu.segurapp.R
 
-fun openAppPermissionsScreen(activity: FragmentActivity){
+fun openAppPermissionsScreen(activity: FragmentActivity) {
     val intent = Intent()
     intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
     val uri: Uri = Uri.fromParts("package", activity.getString(R.string.package_name), null)
     intent.data = uri
     activity.startActivity(intent)
 }
+
+fun getAppPermissions(): Array<String> = arrayOf(Manifest.permission.CAMERA,
+    Manifest.permission.READ_CONTACTS,
+    Manifest.permission.CALL_PHONE,
+    Manifest.permission.ACCESS_COARSE_LOCATION,
+    Manifest.permission.ACCESS_FINE_LOCATION)
