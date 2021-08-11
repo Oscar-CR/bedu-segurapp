c23c168c401b7e251c422f4d3928455354f5adb2
Antonio Labra <antonio_labra@hotmail.com>
2021-08-11 18:36:43 -0500

    Validador de la lista de contactos usando shared preferences, se creo una funcion para implementar alertDialogMaker y navega a home a traves de una función (navigateToHome), es importante mencionar que si el usuario no cumple con contactos de confianza o un mensaje de ayuda y un número telefónico no podrá continuar

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedDoneFragment.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedDoneFragment.kt
@@ -1,25 +1,36 @@
 package org.bedu.segurapp.ui.getStarted
 
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.viewpager2.widget.ViewPager2
 import com.airbnb.lottie.LottieAnimationView
+import com.google.gson.Gson
 import org.bedu.segurapp.R
+import org.bedu.segurapp.helpers.alertDialogMaker
+import org.bedu.segurapp.helpers.movePrevious
+import org.bedu.segurapp.helpers.setSharedPreferences
+import org.bedu.segurapp.models.Contacts
+import org.bedu.segurapp.models.User
 import org.bedu.segurapp.ui.home.HomeActivity
 
-
 class GetStartedDoneFragment : Fragment() {
+
+    private lateinit var preferences: SharedPreferences
     private lateinit var mainAnimation: LottieAnimationView
     private lateinit var btnAccept: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_get_started_done, container, false)
@@ -29,9 +40,51 @@ class GetStartedDoneFragment : Fragment() {
         return view
     }
 
+
+    override fun onPause() {
+        btnAccept.isEnabled = false
+        super.onPause()
+    }
+
+    override fun onResume() {
+        processValidation()
+        super.onResume()
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private fun isAValidUser(): Boolean {
+        preferences = setSharedPreferences(requireActivity())
+        val userString =
+            preferences.getString(resources.getString(R.string.shared_preferences_current_user),
+                null)
+        val userObject = if (userString != null && userString != "") Gson().fromJson(userString,
+            User::class.java) else null
+        return userObject != null && userObject.alertMessage != "" && userObject.telephoneNumber != "" && userObject.telephoneNumber.length == 10
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private fun validateSafeContactsList(): Boolean {
+        val preferences = setSharedPreferences(requireActivity())
+        val safeContactsListString =
+            preferences.getString(resources.getString(R.string.shared_preferences_safe_contact_list),
+                null)
+        val safeContactsList =
+            if (safeContactsListString != null && safeContactsListString != "") Gson().fromJson(
+                safeContactsListString,
+                Array<Contacts>::class.java) else null
+        return safeContactsList != null && safeContactsList.isNotEmpty()
+    }
+
     private fun initComponents(view: View) {
         mainAnimation = view.findViewById(R.id.main_animation)
         btnAccept = view.findViewById(R.id.btn_accept)
+    }
+
+    private fun processValidation() {
+        if (!isAValidUser()) {
+            btnAccept.isEnabled = false
+            createDialog()
+        } else btnAccept.isEnabled = true
     }
 
     private fun setAnimation() {
@@ -42,9 +95,27 @@ class GetStartedDoneFragment : Fragment() {
 
     private fun btnAcceptClickListener() {
         btnAccept.setOnClickListener {
-            val intent = Intent(requireActivity(), HomeActivity::class.java)
-            startActivity(intent)
-            requireActivity().finish()
+            if (validateSafeContactsList()) {
+                navigateToHome()
+            }
         }
+    }
+
+    private fun createDialog() {
+        alertDialogMaker(requireContext(),
+            R.string.incomplete_fields_title,
+            R.string.incomplete_fields,
+            true)
+            .setPositiveButton(getString(R.string.permission_configuration_hint)) { _: DialogInterface, _: Int ->
+                val mPager = (activity as GetStartedActivity).findViewById<ViewPager2>(R.id.pager)
+                if (mPager != null) movePrevious(mPager)
+            }
+            .show()
+    }
+
+    private fun navigateToHome() {
+        val intent = Intent(requireActivity(), HomeActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
     }
 }
