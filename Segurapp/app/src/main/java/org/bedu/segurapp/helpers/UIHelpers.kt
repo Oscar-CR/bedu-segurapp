f8c307afb7740ed7342c66eebc5379ac18a57ad7
Antonio Labra <antonio_labra@hotmail.com>
2021-08-11 18:26:26 -0500

    Creación de funciones genéricas para el uso de SnackBars y AlertDialog

--- Segurapp/app/src/main/java/org/bedu/segurapp/helpers/UIHelpers.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/helpers/UIHelpers.kt
@@ -1,9 +1,11 @@
 package org.bedu.segurapp.helpers
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.snackbar.Snackbar
 
 fun moveNext(mPager: ViewPager2) {
     mPager.currentItem = mPager.currentItem + 1
@@ -16,4 +18,36 @@ fun movePrevious(mPager: ViewPager2) {
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun snackBarMaker(
+    context: Context,
+    view: View,
+    messageId: Int,
+    snackBarLength: Int,
+    extras: List<String?> = ArrayList(),
+): Snackbar {
+
+    return if (extras.isEmpty()) {
+        Snackbar.make(view, context.getString(messageId),
+            snackBarLength)
+    } else {
+        Snackbar.make(view, context.getString(messageId, extras.joinToString()),
+            snackBarLength)
+    }
+
+}
+
+
+fun alertDialogMaker(
+    context: Context,
+    titleId: Int,
+    messageId: Int,
+    cancelable: Boolean = true,
+): AlertDialog.Builder {
+    val builder = AlertDialog.Builder(context)
+    builder.setTitle(context.getString(titleId))
+    builder.setMessage(context.getString(messageId))
+    builder.setCancelable(cancelable)
+    return builder
 }
