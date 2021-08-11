5cbe90ea55931bf172c2beb4429ada89528ef9b9
Antonio Labra <antonio_labra@hotmail.com>
2021-08-11 18:27:59 -0500

    Refactoring de GetStartedActivity para utilizar la función snackBarMaker y la lista de permisos

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedActivity.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedActivity.kt
@@ -1,6 +1,5 @@
 package org.bedu.segurapp.ui.getStarted
 
-import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
@@ -9,16 +8,19 @@ import com.google.android.material.snackbar.Snackbar
 import org.bedu.segurapp.helpers.movePrevious
 import org.bedu.segurapp.R
 import org.bedu.segurapp.adapters.ViewPagerAdapter
+import org.bedu.segurapp.helpers.getAppPermissions
+import org.bedu.segurapp.helpers.snackBarMaker
 
 class GetStartedActivity : AppCompatActivity() {
+
     private lateinit var mPager: ViewPager2
     private val mAdapter by lazy { ViewPagerAdapter(this) }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
-        mPager = findViewById(R.id.pager)
-        mPager.adapter = mAdapter
+        initComponents()
+        setViewPagerAdapter()
         requestPermissions()
     }
 
@@ -27,15 +29,23 @@ class GetStartedActivity : AppCompatActivity() {
         else finish()
     }
 
+    private fun initComponents(){
+        mPager = findViewById(R.id.pager)
+    }
+
+    private fun setViewPagerAdapter(){
+        mPager.adapter = mAdapter
+    }
+
     private fun requestPermissions() {
-        ExcuseMe.couldYouGive(this).permissionFor(Manifest.permission.CAMERA,
-            Manifest.permission.READ_CONTACTS, Manifest.permission.CALL_PHONE, Manifest.permission.LOCATION_HARDWARE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
-        ) {
-            val mSnackBarMessage =
-                if (it.denied.size > 0) getString(R.string.denied_permissions_message_hint) else getString(R.string.granted_permissions_message_hint)
-            val mSnackBar = Snackbar.make(findViewById(android.R.id.content), mSnackBarMessage,
-                Snackbar.LENGTH_LONG)
-            mSnackBar.show()
+        ExcuseMe.couldYouGive(this).permissionFor(*getAppPermissions()) {
+            val mSnackBarMessageId =
+                if (it.denied.size > 0) R.string.denied_permissions_message_hint else R.string.granted_permissions_message_hint
+            snackBarMaker(this,
+                findViewById(android.R.id.content),
+                mSnackBarMessageId,
+                Snackbar.LENGTH_LONG).show()
         }
     }
+
 }
