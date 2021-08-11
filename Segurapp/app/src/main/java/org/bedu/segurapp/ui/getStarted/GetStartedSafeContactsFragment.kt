ffdbf572192e7bcc631f3c60fbba80c34c380f61
Antonio Labra <antonio_labra@hotmail.com>
2021-08-11 18:34:13 -0500

    Uso de refactoring para implementar snackBarMaker y almacenar la lista de contactos de confianza (safeContactsList) con shared preferences

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedSafeContactsFragment.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/getStarted/GetStartedSafeContactsFragment.kt
@@ -24,13 +24,14 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView.OnEditorActionListener
 import androidx.activity.result.contract.ActivityResultContracts
 import com.araujo.jordan.excuseme.ExcuseMe
+import com.google.gson.Gson
 import org.bedu.segurapp.helpers.*
 import org.bedu.segurapp.utils.getNameByUri
 import org.bedu.segurapp.utils.getPhoneByUri
 
-
 class GetStartedSafeContactsFragment : Fragment() {
 
+    private lateinit var mPager: ViewPager2
     private lateinit var chipsContainer: ChipGroup
     private lateinit var btnSafeContacts: Button
     private lateinit var btnNext: Button
@@ -51,7 +52,26 @@ class GetStartedSafeContactsFragment : Fragment() {
         return view
     }
 
+    override fun onPause() {
+        if (safeContactsList.isEmpty() && mPager.currentItem == 3) {
+            movePrevious(mPager)
+            setSafeContactMessage()
+        }
+
+        super.onPause()
+    }
+
+    private val resultLauncher = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intentResult: Intent? = result.data
+                intentResult?.data?.let {
+                    renderContact(it)
+                }
+            }
+        }
+
     private fun initComponents(view: View) {
+        mPager = (activity as GetStartedActivity).findViewById(R.id.pager)
         chipsContainer = view.findViewById(R.id.chips_container)
         btnSafeContacts = view.findViewById(R.id.btn_safe_contacts)
         btnNext = view.findViewById(R.id.btn_next)
@@ -68,15 +88,15 @@ class GetStartedSafeContactsFragment : Fragment() {
                     openContactsList()
                 } else {
 
-                    val snackBar = Snackbar
-                        .make(requireView(),
-                            getString(R.string.denied_read_contacts_permission_message_hint),
-                            Snackbar.LENGTH_INDEFINITE)
+                    snackBarMaker(requireContext(),
+                        requireView(),
+                        R.string.denied_read_contacts_permission_message_hint,
+                        Snackbar.LENGTH_INDEFINITE)
                         .setAction(getString(R.string.permission_configuration_hint)) {
                             openAppPermissionsScreen(requireActivity())
                         }
+                        .show()
 
-                    snackBar.show()
                 }
             }
         }
@@ -85,7 +105,7 @@ class GetStartedSafeContactsFragment : Fragment() {
     private fun txtNumberActionListener() {
         txtNumber.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                if (makeValidations(arrayOf(txtContactName, txtNumber), requireContext())) {
+                if (makeFormValidations(arrayOf(txtContactName, txtNumber), requireContext())) {
                     addSafeContact(Contacts(R.drawable.unknown,
                         txtContactName.text.toString(),
                         txtNumber.text.toString()))
@@ -99,26 +119,19 @@ class GetStartedSafeContactsFragment : Fragment() {
 
     private fun btnNextClickListener() {
         btnNext.setOnClickListener {
-            if (safeContactsList.isNotEmpty()) {
-                val mPager = (activity as GetStartedActivity).findViewById<ViewPager2>(R.id.pager)
-                if (mPager != null) moveNext(mPager)
-            } else {
-                Snackbar.make(requireView(),
-                    resources.getString(R.string.required_safe_contacts_hint),
-                    Snackbar.LENGTH_LONG)
-                    .show()
-            }
+            if (safeContactsList.isNotEmpty()) moveNext(mPager)
+            else setSafeContactMessage()
         }
     }
 
     private fun addSafeContact(safeContact: Contacts) {
         safeContactsList.add(safeContact)
+        updateContactList()
         chipsContainer.addView(createChip(safeContact))
         clearForm(arrayOf(txtContactName, txtNumber))
     }
 
     private fun createChip(safeContact: Contacts): Chip {
-
         val foregroundColor = ContextCompat.getColor(requireContext(), R.color.segurapp_black)
         val chip = Chip(requireContext())
         chip.text = safeContact.name
@@ -126,35 +139,27 @@ class GetStartedSafeContactsFragment : Fragment() {
         chip.isCheckable = false
         chip.isCloseIconVisible = true
         chip.isCheckable = false
-        chip.chipBackgroundColor = getColorStateList(requireContext(),
-            R.color.segurapp_gray)
+        chip.chipBackgroundColor = getColorStateList(requireContext(), R.color.segurapp_gray)
         chip.setTextColor(foregroundColor)
-
         chip.setOnCloseIconClickListener {
             safeContactsList.remove(safeContact)
             chipsContainer.removeView(chip)
         }
-
         return chip
     }
 
+    private fun setSafeContactMessage() {
+        snackBarMaker(requireContext(),
+            requireView(),
+            R.string.required_safe_contacts_hint,
+            Snackbar.LENGTH_LONG)
+            .show()
+    }
 
     private fun openContactsList() {
         val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
         return resultLauncher.launch(intent)
     }
-
-
-    private val resultLauncher =
-        this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == Activity.RESULT_OK) {
-                val intentResult: Intent? = result.data
-                intentResult?.data?.let {
-                    renderContact(it)
-                }
-            }
-        }
-
 
     private fun renderContact(uri: Uri) {
         val name = getNameByUri(uri, requireActivity().contentResolver)
@@ -162,9 +167,22 @@ class GetStartedSafeContactsFragment : Fragment() {
         if (phone != null && name != null) addSafeContact(Contacts(R.drawable.unknown,
             name,
             formatTelephone(phone)))
-        else Snackbar.make(requireView(), getString(R.string.add_safe_contacts_manually_hint, name),
-            Snackbar.LENGTH_INDEFINITE)
-            .setAction(getString(R.string.permission_configuration_hint)) {}
+        else snackBarMaker(requireContext(),
+            requireView(),
+            R.string.add_safe_contacts_manually_hint,
+            Snackbar.LENGTH_INDEFINITE,
+            listOf(name))
+            .setAction(resources.getString(R.string.permission_configuration_hint)) {}
             .show()
     }
+
+    private fun updateContactList() {
+        val preferences = setSharedPreferences(requireActivity())
+        val preferencesEditor = preferences.edit()
+        preferencesEditor.putString(getString(R.string.shared_preferences_safe_contact_list),
+            Gson().toJson(safeContactsList))
+        preferencesEditor.apply()
+    }
 }
+
+
