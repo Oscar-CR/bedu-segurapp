0d25bd797df0f7ae91c402638fa9d97ddefeb5df
Antonio Labra <antonio_labra@hotmail.com>
2021-09-06 03:02:35 -0500

    borrando userIsLogged de MainActivity porque fue removido de helpers.kt

--- Segurapp/app/src/main/java/org/bedu/segurapp/MainActivity.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/MainActivity.kt
@@ -3,8 +3,6 @@ package org.bedu.segurapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.bedu.segurapp.helpers.userIsLogged
-import org.bedu.segurapp.ui.home.HomeActivity
 import org.bedu.segurapp.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +11,7 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.splash_theme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = if (userIsLogged()) Intent(this, HomeActivity::class.java)
-        else Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         this.finish()
     }
