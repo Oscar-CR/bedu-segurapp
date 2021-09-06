09ed362e0cd0b8ecf64535b21dcbb1484f74633c
Antonio Labra <antonio_labra@hotmail.com>
2021-09-06 03:18:48 -0500

    Validación y funcionalidad a BottomSheetForgotPassword.kt

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/forgotPassword/BottomSheetForgotPassword.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/forgotPassword/BottomSheetForgotPassword.kt
@@ -4,10 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import org.bedu.segurapp.R
+import org.bedu.segurapp.helpers.makeFormValidations
 
 class BottomSheetForgotPassword : BottomSheetDialogFragment() {
+
+    private lateinit var metEmail: TextInputEditText
+    private lateinit var mbtnSend: MaterialButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,5 +27,14 @@ class BottomSheetForgotPassword : BottomSheetDialogFragment() {
     }
 
     private fun initComponents(view: View){
+        metEmail = view.findViewById(R.id.met_email)
+        mbtnSend = view.findViewById(R.id.mbtn_send)
+
+        mbtnSend.setOnClickListener {
+            if(makeFormValidations(arrayOf(metEmail), requireContext())){
+                Toast.makeText(requireContext(), "Correo enviado correctamente", Toast.LENGTH_SHORT).show()
+                this.dismiss()
+            }
+        }
     }
 }
