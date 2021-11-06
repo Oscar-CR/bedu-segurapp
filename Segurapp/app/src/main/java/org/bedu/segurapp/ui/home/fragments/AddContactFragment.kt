b8d771d1823ce06f7657bb8e4d68e4c28fce6bce
Antonio Labra <antonio.labra@forteinnovation.mx>
2021-11-05 21:20:54 -0600

    Finalizar tareas agregar nuevo contacto

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/home/fragments/AddContactFragment.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/home/fragments/AddContactFragment.kt
@@ -1,53 +1,90 @@
 package org.bedu.segurapp.ui.home.fragments
 
-import android.content.Intent
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
-import com.airbnb.lottie.LottieAnimationView
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.bedu.segurapp.R
 import org.bedu.segurapp.databinding.FragmentAddContactBinding
-import org.bedu.segurapp.models.AddContactViewModel
-import org.bedu.segurapp.UserLogin
-import org.bedu.segurapp.ui.home.HomeActivity
+import org.bedu.segurapp.helpers.makeFormValidations
+import org.bedu.segurapp.models.*
+import org.bedu.segurapp.networking.RetrofitInstance
+import org.json.JSONObject
 
 class AddContactFragment : Fragment() {
 
-    private lateinit var loginAnimation: LottieAnimationView
-
-    private lateinit var viewModel: AddContactViewModel
     private lateinit var binding: FragmentAddContactBinding
+    private lateinit var mChannel: Channel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    private val mAuth = Firebase.auth
+    private val db = Firebase.firestore
+    private val usersCollection = db.collection("users")
+
+    private val userId = mAuth.currentUser?.uid
+    private var channelId: String? = null
+    private var mNumber: String? = null
+    private var mName: String? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-             binding = DataBindingUtil.inflate(inflater,R.layout.fragment_add_contact,container,
-            false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_add_contact,container, false)
+        getUserInfo()
+        return binding.root
+    }
 
-        viewModel = AddContactViewModel(
-            (requireContext().applicationContext as UserLogin).contactRepository
-        )
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
+        with(binding){
 
-        binding.btnSaveContact.setOnClickListener{
-            Toast.makeText(context ,"Contacto agregado correctamente", Toast.LENGTH_LONG).show()
-            viewModel.newContact()
-            Navigation.findNavController(it).navigate(R.id.navigateToContactsFragment)
+            setAnimation()
+
+            btnSaveContact.setOnClickListener{
+
+                if(mName != null && mNumber != null) {
+                    if (makeFormValidations(arrayOf(etUserAdd, etPhoneAdd), requireContext())) {
+                        sendRequest(
+                            etUserAdd.text.toString(),
+                            etPhoneAdd.text.toString(),
+                            mName ?: "",
+                            mNumber ?: ""
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getUserInfo(){
+
+        userId?.let { idUser ->
+            usersCollection
+                .whereEqualTo("id", idUser)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    if (querySnapshot.documents.isNotEmpty()) {
+                        mNumber = querySnapshot.documents.first().get("telephone").toString()
+                        mName = querySnapshot.documents.first().get("name").toString()
+                        channelId = querySnapshot.documents.first().get("channel.id").toString()
+                    }
+                }
+
         }
 
-        setAnimation()
-        return binding.root
+
     }
 
     private fun setAnimation() {
@@ -58,5 +95,117 @@ class AddContactFragment : Fragment() {
 
     }
 
+    private fun saveContacts(newSafeContact: SafeContact, callback: (Boolean) -> Unit) {
+
+        userId?.let{ idUser ->
+
+            usersCollection
+                .whereEqualTo("id", idUser)
+                .get()
+                .addOnSuccessListener { querySnapshot ->
+                    if (querySnapshot.documents.isNotEmpty()) {
+
+                        val data = querySnapshot.documents.first().get("channel") as HashMap<*, *>
+
+                        mChannel = Gson().fromJson(JSONObject(data).toString(), Channel::class.java)
+                        val userMatchList = mChannel.members.filter { it.telephone == newSafeContact.telephone }
+
+                        if(userMatchList.isEmpty()){
+                            mChannel.members.add(newSafeContact)
+                            usersCollection
+                                .document(idUser)
+                                .update("channel", mChannel)
+                                .addOnCompleteListener { callback(it.isSuccessful) }
+                        }else callback(true)
+                    }
+                }
+
+                .addOnFailureListener { exception ->
+                    Toast.makeText(
+                        requireContext(),
+                        "${exception.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
+
+    }
+
+    private fun sendRequest(contactName: String, contactNumber: String,  mName: String, mNumber: String) {
+        val newSafeContact = SafeContact(contactName, contactNumber)
+
+        usersCollection
+            .whereEqualTo("telephone", contactNumber)
+            .get()
+            .addOnSuccessListener {
+                if (it.documents.isNotEmpty()) {
+
+                    val recipientToken =
+                        it.documents.first().getString("pushNotificationKey")
+
+                    if (recipientToken != null) {
+                        PushNotification(
+                            NotificationData(
+                                getString(
+                                    R.string.notification_send_request_title,
+                                    "$mName ($mNumber)"
+                                ),
+                                getString(R.string.notification_request_description),
+                                channelId.toString()
+                            ),
+                            recipientToken
+                        ).also { pushNotification ->
+                            sendNotification(pushNotification, newSafeContact)
+                        }
+                    }
+
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "$contactName no cuenta con SegurApp",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            }
+
+            .addOnFailureListener { exception ->
+                Toast.makeText(
+                    requireContext(),
+                    "${exception.message}",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+    }
+
+    private fun sendNotification(notification: PushNotification, newSafeContact: SafeContact) = CoroutineScope(Dispatchers.IO).launch {
+            try {
+                print(notification.to)
+
+                val response = RetrofitInstance.api.postNotification(notification)
+                if (response.isSuccessful) {
+
+                    requireActivity().runOnUiThread {
+                        saveContacts(newSafeContact) { saveContactResponse ->
+                            if (saveContactResponse) {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "${newSafeContact.name} será invitado a tus contactos de confianza",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                Navigation.findNavController(requireView()).navigate(R.id.navigateToContactsFragment)
+                            }
+
+                        }
+                    }
+                } else {
+                    Log.e(ContentValues.TAG, response.errorBody().toString())
+                }
+            } catch (e: Exception) {
+                Log.e(ContentValues.TAG, e.toString())
+            }
+        }
 
 }
