bf86af0fd06507e3883d8d7f3ffbef6a4dbcacb8
Antonio Labra <antonio.labra@forteinnovation.mx>
2021-11-07 16:05:43 -0600

    Mejoras en Vista Mi Informacion (activity_myinfo.xml, MyInfoActivity.kt & strings.xml

--- Segurapp/app/src/main/java/org/bedu/segurapp/ui/home/MyInfoActivity.kt
+++ Segurapp/app/src/main/java/org/bedu/segurapp/ui/home/MyInfoActivity.kt
@@ -2,157 +2,66 @@ package org.bedu.segurapp.ui.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Patterns
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_myinfo.*
-import kotlinx.android.synthetic.main.activity_register.*
 import org.bedu.segurapp.R
+import org.bedu.segurapp.databinding.ActivityMyinfoBinding
+import org.bedu.segurapp.helpers.makeFormValidations
 
 class MyInfoActivity : AppCompatActivity() {
 
+    private val binding by lazy {ActivityMyinfoBinding.inflate(layoutInflater)}
     private val db = Firebase.firestore
     private val mAuth = Firebase.auth
-    //private val db = FirebaseFirestore.getInstance()
-    private val users = db.collection("users")
-
-    lateinit var miName: EditText
-    lateinit var miLastName: EditText
-    lateinit var miMessage: EditText
-    lateinit var btnBack: ImageView
-    lateinit var btnGetInfo: Button
-    lateinit var btnUpdate: Button
+    private val usersCollection = db.collection("users")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_myinfo)
-        viewInitializations()
-
-        backButtom()
+        setContentView(binding.root)
+        setupActionBar()
+        backButton()
         getInfo()
+        updateButtonListener()
     }
 
+    private fun setupActionBar() = supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-
-
-    private fun viewInitializations() {
-
-        miName = findViewById(R.id.mi_first_name)
-        //miLastName = findViewById(R.id.mi_last_name)
-        miMessage = findViewById(R.id.mi_message)
-        btnBack = findViewById(R.id.btn_back)
-        btnGetInfo = findViewById(R.id.btn_Get_Info)
-        btnUpdate = findViewById(R.id.btn_Update)
-
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-    }
-
-    private fun validateInput(): Boolean {
-        if (miName.text.toString() == "") {
-            miName.error = "Por favor escribe tu nombre"
-            return false
-        }
-
-        /*if (miLastName.text.toString() == "") {
-            miLastName.error = "Por favor escribe tu apellido"
-            return false
-        }*/
-
-        if (miMessage.text.toString() == "") {
-            miMessage.error = "Por favor escribe tu mensaje de ayuda"
-            return false
-        }
-
-        return true
-    }
-
-
-    fun performEditProfile(view: View) {
-        btnUpdate.setOnClickListener {
-
-            if (validateInput()) {
-                miName.text.toString()
-                //miLastName.text.toString()
-                miMessage.text.toString()
-
-                val users = db.collection("users")
-                val updateData = hashMapOf(
-                    "name" to mi_first_name.text.toString(),
-                    //"last" to "",
-                    "message" to mi_message.text.toString()
-                )
-                users.document(mAuth.currentUser?.uid.toString()).update("message", mi_message.text.toString()).addOnCompleteListener{
-                        response ->
-                    if (response.isSuccessful) {
-                        Toast.makeText(this, "Has actualizado tu perfil", Toast.LENGTH_SHORT).show()
-                        val bundle = Bundle()
-                        val returnhome = Intent(this, HomeActivity::class.java).apply { putExtras(bundle) }
-                        startActivity(returnhome)
-                    }
-                }
-
-
-
-            }
-        }
-    }
-
-    private fun backButtom() {
-        btnBack.setOnClickListener {
+    private fun backButton() {
+        binding.btnBack.setOnClickListener {
             val i = Intent(this, HomeActivity::class.java)
             i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(i)
         }
     }
 
-
     private fun getInfo() {
-        btnGetInfo.setOnClickListener {
-            db.collection("users").document(mAuth.currentUser?.uid.toString()).get().addOnSuccessListener {
-                mi_first_name.setText(it.get("name") as String?)
-                mi_message.setText(it.get("message") as String?)
+        usersCollection.document(mAuth.currentUser?.uid.toString()).get()
+            .addOnSuccessListener {
+                if (it.exists()) {
+                    binding.edEmail.setText(it.get("name") as String?)
+                    binding.edMessage.setText(it.get("message") as String?)
+                }
+            }
+    }
+
+    private fun updateButtonListener(){
+        binding.btnUpdate.setOnClickListener {
+            if (makeFormValidations(arrayOf(binding.edMessage), applicationContext)) {
+                usersCollection.document(mAuth.currentUser?.uid.toString()).update("message", binding.edMessage.text.toString()).addOnCompleteListener { response ->
+                    if (response.isSuccessful) {
+                        Toast.makeText(applicationContext, getString(R.string.success_update_profile), Toast.LENGTH_SHORT).show()
+                        val bundle = Bundle()
+                        val returnHome = Intent(this, HomeActivity::class.java).apply { putExtras(bundle) }
+                        startActivity(returnHome)
+                    }
+                }
             }
         }
     }
 
-
-
-    /*
-
-    private fun updateInfo() {
-        btnUpdate.setOnClickListener{
-            val users = db.collection("users")
-
-            val updateData = hashMapOf(
-                "name" to mi_first_name.text.toString(),
-                //"last" to "",
-                "message" to mi_message.text.toString()
-            )
-            users.document("").set(updateData)
-        }
-
-    }
-*/
-
-
-    //private fun showInfo(name: String, message: String){
-    //users.whereEqualTo("users.name", users).get()
-
-    //}
 }
 
 
